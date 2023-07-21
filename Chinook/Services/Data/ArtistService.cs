fae20c2856f9b7a241ee9d19acd9efe7cda6f9bc
isharaLakshitha <ishara.lakshitha@cube.global>
2023-07-21 19:18:33 +0530

    Code cleanup and remove unwanted catch blocks

--- Chinook/Services/Data/ArtistService.cs
+++ Chinook/Services/Data/ArtistService.cs
@@ -23,24 +23,37 @@ public class ArtistService : IArtistService
         var artist = await dbContext.Artists.Include(p => p.Albums).SingleOrDefaultAsync(a => a.ArtistId == artistId);
         if (artist == null) return null;
         // Data Mapping using automapper
-        var clientArtist = _mapper.Map<ArtistDto>(artist);        
-        return clientArtist;
+        try
+        {
+            var clientArtist = _mapper.Map<ArtistDto>(artist);
+            return clientArtist;
 
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error occurred while mapping Artist entity to ArtistDto.", ex);
+        }
     }
 
-    public  List<ArtistDto> GetClientArtists()
+    public List<ArtistDto> GetClientArtists()
     {
         using var dbContext = _dbFactory.CreateDbContext();
         var updatedArtistList = new List<ArtistDto>();
 
-        foreach (var artist in dbContext.Artists.Include(a => a.Albums))
+        try
         {
-            var clientArtist = _mapper.Map<ArtistDto>(artist);
-            updatedArtistList.Add(clientArtist);
+            foreach (var artist in dbContext.Artists.Include(a => a.Albums))
+            {
+                var clientArtist = _mapper.Map<ArtistDto>(artist);
+                updatedArtistList.Add(clientArtist);
+            }
+
+            return updatedArtistList;
         }
-
-        return updatedArtistList;
-
+        catch (Exception ex)
+        {
+            throw new Exception("Error occurred while retrieving artists.", ex);
+        }
     }
 
     // Keep the existing GetAlbumsForArtist method as it already returns DB model
--- Chinook/Services/Data/PlayListService.cs
+++ Chinook/Services/Data/PlayListService.cs
@@ -16,92 +16,114 @@ public class PlayListService : IPlayListService
         _mapper = mapper;
     }
 
-    public async Task<List<Chinook.ClientModels.Playlist>> GetExistingPlaylists(string CurrentUserId)
+    public async Task<List<ClientModels.Playlist>> GetExistingPlaylists(string CurrentUserId)
     {
-
-        using var dbContext = _dbFactory.CreateDbContext();
-        // Retrieve the existing playlists for the current user
-        var existingPlaylists = await dbContext.UserPlaylists
-            .Where(up => up.UserId == CurrentUserId)
-            .Select(up => up.Playlist)
-            .ToListAsync();
-
-        // Data mapping using Automapper
-        var Clientplaylist = _mapper.Map<List<Chinook.ClientModels.Playlist>>(existingPlaylists);
-
-
-        // Check if the "Favourite" playlist exists in the list
-        var favouritePlaylist = Clientplaylist.FirstOrDefault(p => p.Name == "Favorites");
-        if (favouritePlaylist != null)
+        try
         {
-            // Remove the "Favourite" playlist from its current position and insert it at the beginning
-            Clientplaylist.Remove(favouritePlaylist);
-            Clientplaylist.Insert(0, favouritePlaylist);
-        }
+            using var dbContext = _dbFactory.CreateDbContext();
+            // Retrieve the existing playlists for the current user
+            var existingPlaylists = await dbContext.UserPlaylists
+                .Where(up => up.UserId == CurrentUserId)
+                .Select(up => up.Playlist)
+                .ToListAsync();
 
-        return Clientplaylist;
+            // Data mapping using Automapper
+            var Clientplaylist = _mapper.Map<List<Chinook.ClientModels.Playlist>>(existingPlaylists);
+
+
+            // Check if the "Favourite" playlist exists in the list
+            var favouritePlaylist = Clientplaylist.FirstOrDefault(p => p.Name == "Favorites");
+            if (favouritePlaylist != null)
+            {
+                // Remove the "Favourite" playlist from its current position and insert it at the beginning
+                Clientplaylist.Remove(favouritePlaylist);
+                Clientplaylist.Insert(0, favouritePlaylist);
+            }
+
+            return Clientplaylist;
+        }
+        catch (Exception ex)
+        {
+
+            throw new Exception("Error occurred while retrieving the existing playlists.", ex);
+        }
+       
     }
 
-    public async Task<Chinook.ClientModels.Playlist> GetPlayLists(long PlaylistId, string CurrentUserId)
+    public async Task<ClientModels.Playlist> GetPlayLists(long PlaylistId, string CurrentUserId)
     {
+        try
+        {
+            using var dbContext = _dbFactory.CreateDbContext();
 
-        using var dbContext = _dbFactory.CreateDbContext();
-
-        // Retrieve the playlist
-        var playLists = dbContext.Playlists
-            .Include(a => a.Tracks).ThenInclude(a => a.Album).ThenInclude(a => a.Artist)
-            .Where(p => p.PlaylistId == PlaylistId)
-            .Select(p => new ClientModels.Playlist()
-            {
-                Name = p.Name,
-                Tracks = p.Tracks.Select(t => new ClientModels.PlaylistTrack()
+            // Retrieve the playlist
+            var playLists = dbContext.Playlists
+                .Include(a => a.Tracks).ThenInclude(a => a.Album).ThenInclude(a => a.Artist)
+                .Where(p => p.PlaylistId == PlaylistId)
+                .Select(p => new ClientModels.Playlist()
                 {
-                    AlbumTitle = t.Album.Title,
-                    ArtistName = t.Album.Artist.Name,
-                    TrackId = t.TrackId,
-                    TrackName = t.Name,
-                    IsFavorite = t.Playlists
-                        .Where(p => p.UserPlaylists
-                            .Any(up => up.UserId == CurrentUserId && up.Playlist.Name == "Favorites"))
-                        .Any()
-                }).ToList()
-            })
-            .FirstOrDefault();
+                    Name = p.Name,
+                    Tracks = p.Tracks.Select(t => new ClientModels.PlaylistTrack()
+                    {
+                        AlbumTitle = t.Album.Title,
+                        ArtistName = t.Album.Artist.Name,
+                        TrackId = t.TrackId,
+                        TrackName = t.Name,
+                        IsFavorite = t.Playlists
+                            .Where(p => p.UserPlaylists
+                                .Any(up => up.UserId == CurrentUserId && up.Playlist.Name == "Favorites"))
+                            .Any()
+                    }).ToList()
+                })
+                .FirstOrDefault();
 
-        return playLists;
+            return playLists;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error occurred while retrieving the playlist.", ex);
+        }
+        
     }
 
-    public async Task<Chinook.Models.Playlist> CreateNewPlaylist(string CurrentUserId, string playlistName)
+    public async Task<Playlist> CreateNewPlaylist(string CurrentUserId, string playlistName)
     {
-
-        using var dbContext = _dbFactory.CreateDbContext();
-        var maxPlaylistId = dbContext.Playlists.Max(p => p.PlaylistId);
-        var existPlaylistName = dbContext.UserPlaylists
-            .Where(u => u.UserId == CurrentUserId)
-            .FirstOrDefault(p => p.Playlist.Name == playlistName);
-
-        // Create a new playlist with the provided playlistName and the generated PlaylistId
-        if (existPlaylistName == null)
+        try
         {
-            var playlist = new Chinook.Models.Playlist
+            using var dbContext = _dbFactory.CreateDbContext();
+            var maxPlaylistId = dbContext.Playlists.Max(p => p.PlaylistId);
+            var existPlaylistName = dbContext.UserPlaylists
+                .Where(u => u.UserId == CurrentUserId)
+                .FirstOrDefault(p => p.Playlist.Name == playlistName);
+
+            // Create a new playlist with the provided playlistName and the generated PlaylistId
+            if (existPlaylistName == null)
             {
-                Name = playlistName,
-                PlaylistId = maxPlaylistId + 1
-            };
+                var playlist = new Chinook.Models.Playlist
+                {
+                    Name = playlistName,
+                    PlaylistId = maxPlaylistId + 1
+                };
 
-            // Create a UserPlaylist entry linking the current user to the new playlist
-            var userPlaylist = new UserPlaylist
-            {
-                UserId = CurrentUserId,
-                Playlist = playlist
-            };
+                // Create a UserPlaylist entry linking the current user to the new playlist
+                var userPlaylist = new UserPlaylist
+                {
+                    UserId = CurrentUserId,
+                    Playlist = playlist
+                };
 
-            dbContext.UserPlaylists.Add(userPlaylist);
-            await dbContext.SaveChangesAsync();
+                dbContext.UserPlaylists.Add(userPlaylist);
+                await dbContext.SaveChangesAsync();
 
-            return playlist;
+                return playlist;
+            }
+            return dbContext.Playlists.FirstOrDefault(p => p.PlaylistId == existPlaylistName.PlaylistId);
         }
-        return dbContext.Playlists.FirstOrDefault(p => p.PlaylistId == existPlaylistName.PlaylistId);
+        catch (Exception ex)
+        {
+            throw new Exception("Error occurred while creating a new playlist.", ex);
+        }
+       
 
     }
 
@@ -109,28 +131,36 @@ public class PlayListService : IPlayListService
 
     public async Task<string> AddTrackToPlaylist(long playlistId, long trackId)
     {
-
-        using var dbContext = _dbFactory.CreateDbContext();
-
-        // Find the playlist and track entities
-        var playlist = await dbContext.Playlists
-                  .Include(p => p.Tracks)
-                  .FirstOrDefaultAsync(p => p.PlaylistId == playlistId);
-        var track = await dbContext.Tracks.FindAsync(trackId);
-
-        if (playlist == null || track == null)
+        try
         {
-            // Handle the case where either the playlist or track is not found
-            return null;
+
+            using var dbContext = _dbFactory.CreateDbContext();
+
+            // Find the playlist and track entities
+            var playlist = await dbContext.Playlists
+                      .Include(p => p.Tracks)
+                      .FirstOrDefaultAsync(p => p.PlaylistId == playlistId);
+            var track = await dbContext.Tracks.FindAsync(trackId);
+
+            if (playlist == null || track == null)
+            {
+                // Handle the case where either the playlist or track is not found
+                return null;
+            }
+            if (!playlist.Tracks.Any(t => t.TrackId == trackId))
+            {
+                playlist.Tracks.Add(track);
+            }
+
+            await dbContext.SaveChangesAsync();
+
+            return playlist.Name;
         }
-        if (!playlist.Tracks.Any(t => t.TrackId == trackId))
+        catch (Exception ex)
         {
-            playlist.Tracks.Add(track);
+            throw new Exception("Error occurred while adding a track to the playlist.", ex);
         }
 
-        await dbContext.SaveChangesAsync();
-
-        return playlist.Name;
 
     }
 }
