c04c1734eb6a3d312478fc9393fc9e734dc0819b
isharaLakshitha <ishara.lakshitha@cube.global>
2023-07-24 14:46:15 +0530

    Name Change

--- Chinook/Mapper/MappingProfile.cs
+++ Chinook/Mapper/MappingProfile.cs
@@ -1,18 +1,16 @@
 ﻿using AutoMapper;
-using Chinook.ClientModels;
-using Chinook.Models;
 namespace Chinook.Mapper;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Artist, Artist>()
+        CreateMap<Models.Artist,ClientModels.Artist>()
             .ForMember(dest => dest.NumberOfAlbums, opt => opt.MapFrom(src => src.Albums.Count));
 
         CreateMap<Models.Playlist, ClientModels.Playlist>();
 
-        CreateMap<Track, ClientModels.PlaylistTrack>()
+        CreateMap<Models.Track, ClientModels.PlaylistTrack>()
             .ForMember(dest => dest.TrackName, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.AlbumTitle, opt => opt.MapFrom(src => src.Album.Title))
             .ForMember(dest => dest.ArtistName, opt => opt.MapFrom(src => src.Album.Artist.Name));
